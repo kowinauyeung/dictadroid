0d9d628488a6fa066cc5ab0a8ffeb3c5cefb0db7
kowin <kowinauyeung@gmail.com>
2017-10-02 23:23:51 +0800

    Add vocab edit popup to vocab page

--- src/pages/Vocab.jsx
+++ src/pages/Vocab.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import NavBar from '../components/NavBar';
 import BackButton from '../components/BackButton';
+import EditVocabForm from '../components/EditVocabForm';
 import Speech from '../utils/Speech';
 import './Vocab.css';
 
@@ -44,71 +45,96 @@ const defaultProps = {
   },
 };
 
-function Vocab(props) {
-  const { match, vocab, book, lesson } = props;
-  return (
-    <div className="vocab page">
-      <NavBar
-        pageName={`${book.title} - ${lesson.title}`}
-        left={<BackButton to={`/lessons/${match.params.lessionId}/vocabs`} text="back" />}
-      />
-      <div className="page-inner">
-        <div className="content-block">
-          <div className="content-block-inner">
-            <p className="text-center vocab-display">{vocab.vocab}</p>
-            {vocab.pron ? <p className="text-center pron-display">{vocab.pron}</p> : ''}
-            <p className="text-center">
-              <i
-                className="icon ion-ios-volume-high btn-pron"
-                role="presentation"
-                onClick={() => { Speech.pron(vocab, book.lang); }}
-              />
-            </p>
-            <p className="text-center type-display">[{vocab.type}]</p>
-            <div className="line" />
-            <p className="text-center translate-display">{vocab.translation}</p>
-            {
-              vocab.tags.length <= 0 ?
-                ''
-                :
-                (
-                  <div className="text-center tags-display">
-                    {
-                      vocab.tags.map(tag => (
-                        <div
-                          key={`tag-${tag}`}
-                          className="chip"
-                        >
-                          <div className="chip-label">{tag}</div>
-                        </div>
-                      ))
-                    }
-                  </div>
-                )
-            }
-            <p>
-              <a
-                href={`http://www.google.com/images?q=${vocab.vocab}`}
-                className="button"
-                target="_blank"
-              >
-                Google image
-              </a>
-            </p>
-            <p>
-              <a
-                href={`https://www.japandict.com/?s=${vocab.vocab}`}
-                className="button"
-                target="_blank"
-              >
-                Dictionary
-              </a>
-            </p>
+class Vocab extends Component {
+  constructor() {
+    super();
+    this.state = {
+      editMode: false,
+    };
+    this.startEditVocab = this.startEditVocab.bind(this);
+    this.endEditVocab = this.endEditVocab.bind(this);
+  }
+
+  startEditVocab() {
+    this.setState({ editMode: true });
+  }
+
+  endEditVocab() {
+    this.setState({ editMode: false });
+  }
+
+  render() {
+    const { match, vocab, book, lesson } = this.props;
+    const { editMode } = this.state;
+    return (
+      <div className="vocab page">
+        <NavBar
+          pageName={`${book.title} - ${lesson.title}`}
+          left={<BackButton to={`/lessons/${match.params.lessionId}/vocabs`} text="back" />}
+          right={<div onClick={this.startEditVocab} role="presentation">Edit</div>}
+        />
+        <div className="page-inner">
+          <div className="content-block">
+            <div className="content-block-inner">
+              <p className="text-center vocab-display">{vocab.vocab}</p>
+              {vocab.pron ? <p className="text-center pron-display">{vocab.pron}</p> : ''}
+              <p className="text-center">
+                <i
+                  className="icon ion-ios-volume-high btn-pron"
+                  role="presentation"
+                  onClick={() => { Speech.pron(vocab, book.lang); }}
+                />
+              </p>
+              <p className="text-center type-display">[{vocab.type}]</p>
+              <div className="line" />
+              <p className="text-center translate-display">{vocab.translation}</p>
+              {
+                vocab.tags.length <= 0 ?
+                  ''
+                  :
+                  (
+                    <div className="text-center tags-display">
+                      {
+                        vocab.tags.map(tag => (
+                          <div
+                            key={`tag-${tag}`}
+                            className="chip"
+                          >
+                            <div className="chip-label">{tag}</div>
+                          </div>
+                        ))
+                      }
+                    </div>
+                  )
+              }
+              <p>
+                <a
+                  href={`http://www.google.com/images?q=${vocab.vocab}`}
+                  className="button"
+                  target="_blank"
+                >
+                  Google image
+                </a>
+              </p>
+              <p>
+                <a
+                  href={`https://www.japandict.com/?s=${vocab.vocab}`}
+                  className="button"
+                  target="_blank"
+                >
+                  Dictionary
+                </a>
+              </p>
+            </div>
           </div>
         </div>
+        <EditVocabForm
+          targetVocab={editMode ? vocab : undefined}
+          hide={this.endEditVocab}
+        />
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 Vocab.propTypes = propTypes;
