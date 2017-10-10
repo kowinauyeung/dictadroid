bb7ad3b259761fd7c020ecf285b9dd2f901b9c17
kowin <kowinauyeung@gmail.com>
2017-10-10 10:07:19 +0800

    Improve speech and speak without special character

--- src/utils/Speech.js
+++ src/utils/Speech.js
@@ -7,25 +7,27 @@ const langMap = {
   ja: 'Japanese Female',
 };
 
+const filterSpecialChar = (txt => txt.replace('～', ''));
+
 Speech.pron = (vocab, lang) => {
   const voice = langMap[lang];
   const text = (vocab.useSpeech && vocab.pron) ? vocab.pron : vocab.vocab;
   if (responsiveVoice.isPlaying()) responsiveVoice.cancel();
   responsiveVoice.cancel();
-  responsiveVoice.speak(text, voice);
+  responsiveVoice.speak(filterSpecialChar(text), voice);
 };
 
 Speech.trans = (vocab, lang) => {
   const voice = langMap[lang];
   if (responsiveVoice.isPlaying()) responsiveVoice.cancel();
   responsiveVoice.cancel();
-  responsiveVoice.speak(vocab.translation, voice);
+  responsiveVoice.speak(filterSpecialChar(vocab.translation), voice);
 };
 
 Speech.speak = (text, lang) => {
   const voice = langMap[lang];
   responsiveVoice.cancel();
-  responsiveVoice.speak(text, voice);
+  responsiveVoice.speak(filterSpecialChar(text), voice);
 };
 
 export default Speech;
