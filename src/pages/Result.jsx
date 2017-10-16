4a74bcec06c8bdf127d68eccb9f6a39924549657
kowin <kowinauyeung@gmail.com>
2017-10-16 18:15:57 +0800

    Fix scroll problem in result page

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import moment from 'moment';
 import 'moment/locale/zh-hk';
@@ -42,107 +42,130 @@ const momentLangMap = {
   ja: 'ja',
 };
 
-function renderNoData(isFetchingResults, LANG) {
-  return (
-    <div className="real-center">
-      <p className="text-center grey">
-        {isFetchingResults ? LANG.LOADING : LANG.NO_RESULTS_MSG}
-      </p>
-    </div>
-  );
-}
+let scrollTop = 0;
+let previousResultId = null;
 
-const renderResult = (results, match, book, LANG, lang) => {
-  const resultId = match.params.resultId;
-  const result = results[resultId];
-  moment.locale(momentLangMap[lang]);
-  return (
-    <div className="ks-card-header-pic">
-      <div className="card-content">
-        <div className="card-content-inner">
-          <h2>{book.title}</h2>
-          <p className="grey">{LANG.LESSONS}: {result.subject}</p>
-          <p className="grey">
-            {LANG.TRAINING_TYPE}: {LANG[result.trainType.toUpperCase()]},&nbsp;
-            {LANG.LANGUAGE}: {Lang[book.lang]}
-          </p>
-          <p className="grey">
-            {moment(result.createDt).calendar()},&nbsp;
-            {LANG.SCORE}: {result.correctAnswer}/{result.vocabs.length}
-          </p>
-        </div>
-        <div className="list-block">
-          <ul>
-            <li>
-              <div className="item-content">
-                <div className="item-inner bold">
-                  <div className="item-title">{LANG.QUESTIONS}</div>
-                  <div className="item-after">{LANG.YOUR_ANSWERS}</div>
+class Result extends Component {
+  constructor({ match }) {
+    super();
+    const resultId = match.params.resultId;
+    if (previousResultId !== resultId) scrollTop = 0;
+    previousResultId = resultId;
+    this.scrollContainer = null;
+  }
+
+  componentDidMount() {
+    if (this.scrollContainer) this.scrollContainer.scrollTop = scrollTop;
+  }
+
+  componentWillUnmount() {
+    if (this.scrollContainer) scrollTop = this.scrollContainer.scrollTop;
+  }
+
+  renderNoData() {
+    const { isFetchingResults, LANG } = this.props;
+    return (
+      <div className="real-center">
+        <p className="text-center grey">
+          {isFetchingResults ? LANG.LOADING : LANG.NO_RESULTS_MSG}
+        </p>
+      </div>
+    );
+  }
+
+  renderResult() {
+    const { results, match, book, LANG, lang } = this.props;
+    const resultId = match.params.resultId;
+    const result = results[resultId];
+    moment.locale(momentLangMap[lang]);
+    return (
+      <div className="ks-card-header-pic" ref={(ref) => { this.scrollContainer = ref; }}>
+        <div className="card-content">
+          <div className="card-content-inner">
+            <h2>{book.title}</h2>
+            <p className="grey">{LANG.LESSONS}: {result.subject}</p>
+            <p className="grey">
+              {LANG.TRAINING_TYPE}: {LANG[result.trainType.toUpperCase()]},&nbsp;
+              {LANG.LANGUAGE}: {Lang[book.lang]}
+            </p>
+            <p className="grey">
+              {moment(result.createDt).calendar()},&nbsp;
+              {LANG.SCORE}: {result.correctAnswer}/{result.vocabs.length}
+            </p>
+          </div>
+          <div className="list-block">
+            <ul>
+              <li>
+                <div className="item-content">
+                  <div className="item-inner bold">
+                    <div className="item-title">{LANG.QUESTIONS}</div>
+                    <div className="item-after">{LANG.YOUR_ANSWERS}</div>
+                  </div>
                 </div>
-              </div>
-            </li>
-            {
-              result.vocabs.map(vocab => (
-                <li key={vocab.id}>
-                  <Link
-                    to={`/vocab-card/${parseJSONToURIComponent(plainVocabObject({
-                      ...vocab,
-                      lang: book.lang,
-                    }))}`}
-                  >
-                    <div className="item-content">
-                      <div className="item-inner">
-                        <div className="item-title">
-                          {result.trainType === 'dictation' ? vocab.vocab : vocab.translation}
-                        </div>
-                        <div className={`item-after${
-                          removeStuffInVocab(vocab.vocab) === vocab.answer ? '' : ' fail'
-                        }`}
-                        >
-                          {vocab.answer}
+              </li>
+              {
+                result.vocabs.map(vocab => (
+                  <li key={vocab.id}>
+                    <Link
+                      to={`/vocab-card/${parseJSONToURIComponent(plainVocabObject({
+                        ...vocab,
+                        lang: book.lang,
+                      }))}`}
+                    >
+                      <div className="item-content">
+                        <div className="item-inner">
+                          <div className="item-title">
+                            {result.trainType === 'dictation' ? vocab.vocab : vocab.translation}
+                          </div>
+                          <div className={`item-after${
+                            removeStuffInVocab(vocab.vocab) === vocab.answer ? '' : ' fail'
+                          }`}
+                          >
+                            {vocab.answer}
+                          </div>
                         </div>
                       </div>
-                    </div>
-                  </Link>
-                </li>
-              ))
-            }
-          </ul>
+                    </Link>
+                  </li>
+                ))
+              }
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
-
-function Result(props) {
-  const { isAppReady, isFetchingResults, results, match, book, LANG, lang } = props;
-  const resultId = match.params.resultId;
-  const result = results[resultId];
-
-  if (!isAppReady) {
-    return <Redirect to={`/redirect?url=/results/${resultId}`} />;
+    );
   }
 
-  if (!result && !isFetchingResults) {
-    return <Redirect to="/results" />;
-  }
+  render() {
+    const { isAppReady, isFetchingResults, results, match, LANG } = this.props;
+    const resultId = match.params.resultId;
+    const result = results[resultId];
 
-  return (
-    <div className="history page">
-      <NavBar
-        pageName={LANG.RESULTS}
-        left={<BackButton to="/results" text={LANG.BACK} />}
-      />
-      <div className="page-inner">
-        {
-          !result || Object.keys(results).length <= 0 ?
-            renderNoData(isFetchingResults, LANG)
-            :
-            renderResult(results, match, book, LANG, lang)
-        }
+    if (!isAppReady) {
+      return <Redirect to={`/redirect?url=/results/${resultId}`} />;
+    }
+
+    if (!result && !isFetchingResults) {
+      return <Redirect to="/results" />;
+    }
+
+    return (
+      <div className="history page">
+        <NavBar
+          pageName={LANG.RESULTS}
+          left={<BackButton to="/results" text={LANG.BACK} />}
+        />
+        <div className="page-inner">
+          {
+            !result || Object.keys(results).length <= 0 ?
+              this.renderNoData()
+              :
+              this.renderResult()
+          }
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 Result.propTypes = propTypes;
--- src/pages/Results.jsx
+++ src/pages/Results.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import _ from 'lodash';
 import moment from 'moment';
@@ -10,7 +10,6 @@ import NavBar from '../components/NavBar';
 import BackButton from '../components/BackButton';
 
 const propTypes = {
-  lang: PropTypes.string.isRequired,
   LANG: PropTypes.oneOfType([PropTypes.string, PropTypes.object]).isRequired,
   isFetchingResults: PropTypes.bool.isRequired,
   isAppReady: PropTypes.bool.isRequired,
@@ -27,87 +26,108 @@ const propTypes = {
   ).isRequired,
 };
 
-function renderNoData(isFetchingResults, LANG) {
-  return (
-    <div className="real-center">
-      <p className="text-center grey">
-        {isFetchingResults ? LANG.LOADING : LANG.NO_RESULTS_MSG}
-      </p>
-    </div>
-  );
-}
+let scrollTop = 0;
 
-function orderResults(results) {
-  const res = [];
-  Object.keys(results).forEach((key) => {
-    res.push(results[key]);
-  });
-  return _.orderBy(res, ['createDt'], ['desc']);
-}
-
-function renderList(results, LANG) {
-  return (
-    <div className="list-block">
-      <ul>
-        {
-          orderResults(results).map((result) => {
-            const score = (result.correctAnswer / result.vocabs.length);
-            const isPass = (score >= 0.5);
-            const resultClass = classNames('item-title-after', {
-              fail: !isPass,
-              fullmarks: score >= 1,
-            });
-            return (
-              <li key={result.id}>
-                <div className="item-content">
-                  <div className="item-inner">
-                    <Link to={`/results/${result.id}`}>
-                      <div className="item-title-row">
-                        <div className="item-title">
-                          {_.truncate(result.subject, { length: 16, separator: '' })}
-                        </div>
-                      </div>
-                      <div className="item-subtitle grey">
-                        {LANG[result.trainType.toUpperCase()]}, {moment(result.createDt).calendar()}
-                      </div>
-                    </Link>
-                    <div className={resultClass}>
-                      <span>{LANG.SCORE}: {result.correctAnswer}/{result.vocabs.length}</span>
-                    </div>
-                  </div>
-                </div>
-              </li>
-            );
-          })
-        }
-      </ul>
-    </div>
-  );
-}
-
-function Results(props) {
-  const { isAppReady, isFetchingResults, results, LANG } = props;
-
-  if (!isAppReady) {
-    return <Redirect to="/redirect?url=/results" />;
+class Results extends Component {
+  constructor() {
+    super();
+    this.scrollContainer = null;
   }
 
-  return (
-    <div className="history page">
-      <NavBar
-        pageName={LANG.RESULTS}
-        left={<BackButton to="/" text={LANG.BACK} />}
-      />
-      <div className="page-inner">
-        {
-          Object.keys(results).length <= 0 ?
-            renderNoData(isFetchingResults, LANG)
-            :
-            renderList(results, LANG)
-        }
+  componentDidMount() {
+    this.scrollContainer.scrollTop = scrollTop;
+  }
+
+  componentWillUnmount() {
+    scrollTop = this.scrollContainer.scrollTop;
+  }
+
+  orderResults() {
+    const { results } = this.props;
+    const res = [];
+    Object.keys(results).forEach((key) => {
+      res.push(results[key]);
+    });
+    return _.orderBy(res, ['createDt'], ['desc']);
+  }
+
+  renderNoData() {
+    const { isFetchingResults, LANG } = this.props;
+    return (
+      <div className="real-center">
+        <p className="text-center grey">
+          {isFetchingResults ? LANG.LOADING : LANG.NO_RESULTS_MSG}
+        </p>
       </div>
-    </div>
-  );
+    );
+  }
+
+  renderList() {
+    const { results, LANG } = this.props;
+    return (
+      <div className="list-block">
+        <ul>
+          {
+            this.orderResults(results).map((result) => {
+              const score = (result.correctAnswer / result.vocabs.length);
+              const isPass = (score >= 0.5);
+              const resultClass = classNames('item-title-after', {
+                fail: !isPass,
+                fullmarks: score >= 1,
+              });
+              return (
+                <li key={result.id}>
+                  <div className="item-content">
+                    <div className="item-inner">
+                      <Link to={`/results/${result.id}`}>
+                        <div className="item-title-row">
+                          <div className="item-title">
+                            {_.truncate(result.subject, { length: 16, separator: '' })}
+                          </div>
+                        </div>
+                        <div className="item-subtitle grey">
+                          {LANG[result.trainType.toUpperCase()]},&nbsp;
+                          {moment(result.createDt).calendar()}
+                        </div>
+                      </Link>
+                      <div className={resultClass}>
+                        <span>{LANG.SCORE}: {result.correctAnswer}/{result.vocabs.length}</span>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+              );
+            })
+          }
+        </ul>
+      </div>
+    );
+  }
+
+  render() {
+    const { isAppReady, results, LANG } = this.props;
+
+    if (!isAppReady) {
+      return <Redirect to="/redirect?url=/results" />;
+    }
+
+    return (
+      <div className="history page">
+        <NavBar
+          pageName={LANG.RESULTS}
+          left={<BackButton to="/" text={LANG.BACK} />}
+        />
+        <div className="page-inner" ref={(ref) => { this.scrollContainer = ref; }}>
+          {
+            Object.keys(results).length <= 0 ?
+              this.renderNoData()
+              :
+              this.renderList()
+          }
+        </div>
+      </div>
+    );
+  }
 }
 
 Results.propTypes = propTypes;
